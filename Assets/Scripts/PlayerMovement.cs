a4e309ef93f94531797bf1cc1ff3c5b2456931a6
jamalcwoods <jamalcwoods@gmail.com>
2023-02-04 18:15:02 -0500

    Added Movement
    Forwards (With animation)
    Backwards (With animation)
    Jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     Animator pAnimator;
+    Rigidbody rb;
+    BoxCollider collider;
 
+    [SerializeField]
+    private bool grounded;
+    
+    [SerializeField]
+    private bool jumping;
+    
     void Start()
     {
+        collider = GetComponent<BoxCollider>();
         pAnimator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
 
@@ -17,25 +29,62 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    private void FixedUpdate()
+    {
+        grounded = isGrounded();
+        if (grounded && jumping)
+        {
+            jumping = false;
+        }
+    }
+
     void Move()
     {
         // Move forward
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && !jumping)
         {
-            Debug.Log("Moving forward");
-            pAnimator.SetBool("Walk", true);
+            rb.velocity = new Vector3(1, 0, 0);
+            pAnimator.SetBool("Walk Forwards", true);
         }
-        else 
-            pAnimator.SetBool("Walk", false);
+        else
+        {
+            pAnimator.SetBool("Walk Forwards", false);
+        }
 
         // Move backwards
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !jumping)
         {
-            Debug.Log("Walk back.");
+            rb.velocity = new Vector3(-1, 0, 0);
             pAnimator.SetBool("Walk Backwards", true);
         }
-            
         else
+        {
             pAnimator.SetBool("Walk Backwards", false);
+        }
+
+        //Jump
+        if (Input.GetKey(KeyCode.Space) && grounded && !jumping)
+        {
+            jumping = true;
+            pAnimator.SetBool("Jumping", true);
+            rb.velocity = new Vector3(0, 4, 0);
+        }
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(collider.bounds.center, Vector3.down, collider.bounds.extents.y);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+            if (hit.collider.gameObject.name == "Platform")
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
